06ab3dc7ee61fb8e092985d040f0a02f8e3dfabe
trrrrrys <tsukahararu@gmail.com>
2024-02-08 21:09:41 +0900

    feat: 🎸 update

--- cmd/codecov-example/main.go
+++ cmd/codecov-example/main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"github.com/kujira-company/kujira-sc/internal/cmd1"
 	"github.com/kujira-company/kujira-sc/internal/cmd2"
-	"github.com/kujira-company/kujira-sc/internal/deadlock"
-	"github.com/kujira-company/kujira-sc/internal/race"
 )
 
 func main() {
@@ -12,6 +10,4 @@ func main() {
 	_ = cmd1.Command1(2)
 	_ = cmd2.Command2(1)
 	_ = cmd2.Command2(2)
-	race.Run()
-	deadlock.Run()
 }
